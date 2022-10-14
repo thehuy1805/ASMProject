3b8a297a7af67258cfd69a5a6dd566b694c19001
myndgcc20002 <100873277+myndgcc20002@users.noreply.github.com>
2022-10-14 10:20:17 +0700

    change urlurl

--- models/db.js
+++ models/db.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
 //const url = "mongodb://localhost:27017/EmployeeDB";
 //const url = "mongodb+srv://tmtuan:**************@cluster0.zfovn.mongodb.net/test"
-const url = "mongodb+srv://greenwich:exxGJzEy15meXqaR@cluster0.zfovn.mongodb.net/"
-mongoose.connect(url,{useNewUrlParser:true},(err) => {
-    if(!err){ console.log("MongoDB Connection Succeeded");}
-    else{
+const url = "mongodb+srv://my:admin@123@cluster0.e0p10pr.mongodb.net/test/"
+mongoose.connect(url, { useNewUrlParser: true }, (err) => {
+    if (!err) { console.log("MongoDB Connection Succeeded"); } else {
         console.log("An Error Occured");
-    } 
+    }
 })
 
 require('./employee.model');
