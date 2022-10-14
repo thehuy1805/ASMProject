e5ad7e85babd106e4f5cf0177c2da52597f5f42a
myndgcc20002 <100873277+myndgcc20002@users.noreply.github.com>
2022-10-14 10:38:10 +0700

    Thay url

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //const url = "mongodb://localhost:27017/EmployeeDB";
 //const url = "mongodb+srv://tmtuan:**************@cluster0.zfovn.mongodb.net/test"
-const url = "mongodb+srv://my:admin@123@cluster0.e0p10pr.mongodb.net/test/"
+const url = "mongodb+srv://my:admin@123@cluster0.e0p10pr.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(url, { useNewUrlParser: true }, (err) => {
     if (!err) { console.log("MongoDB Connection Succeeded"); } else {
         console.log("An Error Occured");
